24dcc92cdb1b4328956769b3c2f9342bc509d348
Bejarano03 <69337575+Bejarano03@users.noreply.github.com>
2024-03-13 20:34:50 -0700

    inital commit

---
+++ src/components/ContentA.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as SharedComponent from './SharedComponents';
+
+export const ContentA = () => {
+  return (
+    <div>
+        <p>Curabitur finibus et enim ac rhoncus. Donec semper quam magna, sed ultricies magna interdum egestas. Donec non sem ut tellus tincidunt semper nec gravida quam. Cras luctus, nisi nec dapibus ornare, tortor turpis faucibus est, ultrices ornare quam lectus maximus dolor. Sed eget ipsum magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras feugiat, nulla in commodo congue, metus metus porta dolor, vel imperdiet tortor mi pulvinar urna. Nunc pretium faucibus porttitor. Quisque faucibus condimentum ipsum, eu egestas eros cursus non. Sed consequat sagittis eleifend. Praesent aliquet, nisl non mattis tristique, diam orci cursus lacus, nec lobortis lorem tellus sed sem. Nulla sit amet felis eu magna pulvinar porta ac vel sem.</p>
+        <SharedComponent.Button />
+    </div>
+  )
+}
---
+++ src/components/ContentB.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as SharedComponent from './SharedComponents';;
+
+export const ContentB = () => {
+  return (
+    <div>
+      Etiam augue arcu, consectetur sit amet sagittis sit amet, egestas a felis. Vestibulum feugiat, erat sit amet rhoncus iaculis, nulla lectus vulputate dolor, at imperdiet diam risus eu risus. Donec fermentum metus nunc, sed pellentesque turpis pulvinar at. Nunc sagittis justo dolor, eget scelerisque lacus congue quis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc at sapien ac ex sodales sodales. Cras ut rhoncus odio. Fusce molestie tincidunt nisl eu fermentum. Vivamus porttitor erat vitae consequat tempus. Ut porta suscipit magna sit amet rhoncus. Maecenas sit amet blandit metus. Duis facilisis leo magna, sit amet auctor sapien porta dignissim. Aenean ac maximus urna, et feugiat dui. Integer ac varius dolor, at malesuada enim.
+      <SharedComponent.Button />
+    </div>
+  )
+}
